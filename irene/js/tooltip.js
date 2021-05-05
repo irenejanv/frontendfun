9eef166d56d2b0a4f040affd844ee2b0d635cb6f
irenejanv <72525176+irenejanv@users.noreply.github.com>
2021-05-05 14:56:53 -0400

    update notes js

--- irene/js/tooltip.js
+++ irene/js/tooltip.js
@@ -1,16 +1,16 @@
 // 0. Set things up so you can interact with circles.
 let circle_labels = document.getElementsByClassName('circle');
 let circle = Array.from(circle_labels);
-// to do this for text boxes, (1) svg text elements need a class "dish_text" and them make sure they have an idea that is "dish-0-text". Repeat lines 2-3 and then lines 5-8.
+
 for (let i = 0; i < circle.length; i++) {
   circles[i].addEventListener('mouseenter', showTooltip);
   circles[i].addEventListener('mouseleave', hideTooltip);
 }
 
-// 1. What happens when you mouse over the blue rectangle
+// 1. What happens when you mouse over the circles
 function showTooltip() {
-  // What food are we going to show?
-  // Check the id of the rectangle we're hovering over.
+  // What tooltip are we going to show?
+  // Check the id of the circle we're hovering over.
 
   let id = this.id;
   console.log(id);
@@ -27,13 +27,13 @@ function showTooltip() {
     '150px';
 }
 
-// 2. What happens when you mouse out of the blue rectangle
+// 2. What happens when you mouse out of the circle
 function hideTooltip() {
   let id = this.id;
   console.log(id);
   let circle_number = id.split('-')[1];
 
-  // Show the box the picture is in:
+  // Show the box the tooltip is in:
   document.getElementById('show-tooltip-' + circle_number).style.display =
     'none';
 }
